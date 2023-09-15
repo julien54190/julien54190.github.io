96ea7434aa17f5a88a598d55195986a149f4de59
Julien Lucadello <lucadello.julien@hotmail.fr>
2023-09-15 09:57:11 +0200

    add project clock project table jo

--- 7.clock/correction_minuteur/minuteur_correction.php
+++ 7.clock/correction_minuteur/minuteur_correction.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>minuteur</title>
 </head>
 <body>
+    <div class="flex">
     <p id="minuteur">00:00:00</p>
     <form action="" method="post">
         <label for="heure">Heure(s)</label>
@@ -16,35 +17,66 @@
         <input type="number" name="minute" id="minute" value="0" min="0" max="59" lenght="2">
 
         <label for="seconde">Seconde(s)</label>
-        <input type="number" name="seconde" id="seconde" value="15" min="0" max="59" lenght="2">
+        <input type="number" name="seconde" id="seconde" value="0" min="0" max="59" lenght="2">
 
         <input type="submit" value="Régler">
+        <button type = "button" id="pause" onclick="PauseTimer()">Pause</button>
+        <button type = "button" id="play">Play</button>
+        <button type = "button" id="reset" onclick="ResetTimer()">Reset</button>
+        
+        
     </form>
+    </div>
+    
+
 
     <?php
         echo '    <script>
         var heure = ' . (!empty($_POST["heure"]) ? $_POST["heure"] : '0').'
         var minute = ' . (!empty($_POST["minute"]) ? $_POST["minute"] : '0').'
-        var seconde = ' . (!empty($_POST["seconde"]) ? $_POST["seconde"] : '15').'
-        setInterval (function() {
+        var seconde = ' . (!empty($_POST["seconde"]) ? $_POST["seconde"] : '0').'
+        function timer () {
             document.getElementById("minuteur").innerHTML = `${(heure < 10 ? "0" : "") + heure}:${(minute < 10 ? "0" : "") + minute}:${(seconde < 10 ? "0" : "") + seconde}`
-            if (seconde <= 0) return
-            seconde--
+            if (seconde <= 0 && minute <= 0 && heure <= 0) return
+            
             if (seconde == 0){
                 if (minute > 0) {
-                    seconde = 59
+                    seconde = 60
                     minute--
                 } else {
                     if (heure > 0){
                         heure-- 
                         minute = 59
-                        seconde = 59
+                        seconde = 60
                     }
                 }
             }
-            
-        }, 1000)
-    </script>'
+            seconde--
+        }
+        var interval =  setInterval (timer, 1000)
+        function PauseTimer() {
+            clearInterval(interval)
+            interval = null
+            document.getElementById("minuteur").style.color = "red"
+            document.getElementById("minuteur").style.fontStyle = "italic"
+            document.getElementById("minuteur").style.background = "black"
+        }
+        document.getElementById("play").addEventListener("click", function(){
+            if (interval == null) {
+                interval = setInterval(timer ,1000)
+                document.getElementById("minuteur").style.color = "black"
+                document.getElementById("minuteur").style.background = "blue"
+            }
+
+        })
+        function ResetTimer(){
+            clearInterval(interval)
+            document.getElementById("heure").value = "0"
+            document.getElementById("minute").value = "0"
+            document.getElementById("seconde").value = "0"
+            document.getElementById("minuteur").textContent = "00:00:00"
+        }
+    </script>';
     ?>
 
 </body>
